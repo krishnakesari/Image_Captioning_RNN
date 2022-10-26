36d4e25c53fbd1feb145a50f02eedb8e04593b2c
Krishnkesari <bkesari@worldbank.org>
2022-10-26 15:09:38 -0400

    Encoder need further fixing

--- CNN encoder.py
+++ CNN encoder.py
@@ -11,11 +11,12 @@ print(keras.__version__)
 # %%
 
 # Download text files
-captions_folder = '/Flicker8k_text'
+captions_folder = '/Flicker8k_text/'
 if not os.path.exists(os.path.abspath('.') + captions_folder):
     captions_zip = tf.keras.utils.get_file('Flicker8k_text.zip',
                                            cache_subdir=os.path.abspath('.'),
-                                           origin='https://github.com/jbrownlee/Datasets/releases/download/Flickr8k/Flickr8k_text.zip',
+                                           origin='https://github.com/jbrownlee/Datasets/releases/download/Flickr8k'
+                                                  '/Flickr8k_text.zip',
                                            extract=True)
     os.remove(captions_zip)
 
@@ -24,7 +25,8 @@ images_folder = '/Flicker8k_Dataset/'
 if not os.path.exists(os.path.abspath('.') + images_folder):
     images_zip = tf.keras.utils.get_file('Flicker8k_Dataset.zip',
                                          cache_subdir=os.path.abspath('.'),
-                                         origin='https://github.com/jbrownlee/Datasets/releases/download/Flickr8k/Flickr8k_Dataset.zip',
+                                         origin='https://github.com/jbrownlee/Datasets/releases/download/Flickr8k'
+                                                '/Flickr8k_Dataset.zip',
                                          extract=True)
     PATH = os.path.dirname(images_zip) + images_folder
     os.remove(images_zip)
@@ -33,33 +35,48 @@ else:
 
 
 # %%
+# Create LoadData class that contains three methods to load data
+## "load_text_doc" takes path for "Flicker8k.token.txt" file (It holds image IDs along with their captions)
+## "image_caption_dict" takes 'text' from "load_text_doc" as an argument.
+### (Inside this method, we generate a python dictionary with Image name as key and corresponding caption as values)
+## "train_img_names" takes path of "Flicker8k.trainImages.txt" file (Provides a set of all images in training file)
+
 class LoadData:
     def load_text_doc(self, file_path):
         with open(file_path, 'r') as cf:
             cap_text = cf.read()
-        return cap_text
+            return cap_text
 
     def image_caption_dict(self, text):
-        caption_mapping = {}
+        caption_mappings = {}  # Define dictionary
 
-        lines = text.split('\n')
+        lines = text.split('\n')  # Split text into lines
         for line in lines:
-            line_split = line.split('\t')
-            if len(line_split) < 2:
+            line_split = line.split('\t')  # Image name and caption are seperated by a 'tab' so we are splitting lines
+            # by a tab
+            if len(line_split) < 2:  # Checking if each image has two lines
                 continue
             else:
+                # Reference sentence for below code :
+                # "1022454332_6af2c1449a.jpg#0	A child and a woman are at waters edge in a big city ."
                 image_meta, caption = line_split
                 raw_image_name, caption_number = image_meta.split('#')
-                image_name = raw_image_name.split('.')[0]
+                image_name = raw_image_name.split('.')[
+                    0]  # In case of multiple captions just grab the first caption only
 
+                # To check if this is the first caption of the image,
+                # else we will create a new list for that image and add caption.
                 if int(caption_number) == 0:
-                    caption_mapping[image_name] = [caption]
+                    caption_mappings[image_name] = [caption]
                 else:
-                    caption_mapping[image_name].append(caption)
+                    caption_mappings[image_name].append(caption)
 
-                return caption_mapping
+                return caption_mappings
 
-    def train_image_names(self, file_path):
+    # In this method, we will select the subset of images to be used from our full training dataset. (we have three data
+    # files that can be used for training, validation and testing purposes)
+
+    def train_img_names(self, file_path):
         data = []
         with open(file_path, 'r') as fp:
             text = fp.read()
@@ -67,27 +84,36 @@ class LoadData:
             for line in lines:
                 if len(line) < 1:
                     continue
-                train_image_name = line.split('.')[0]
-                data.append(train_image_name)
-                return data
+                train_img_name = line.split('.')[0]
+                data.append(train_img_name)
+
+        return data
 
 
 # %%
 # Setting up preprocessing images with InceptionV3
 class PrepProcessImages:
 
-    def load_image(self, path):
+    def load_image(self, path):  # Takes path of an image
         image = tf.io.read_file(path)
         image = tf.image.decode_jpeg(image, channels=3)
         image = tf.image.resize(image, (299, 299))
-        image = keras.applications.inception_V3(include_top=False, weights='imagenet')
+        image = tf.keras.applications.inception_v3.preprocess_input(image)
 
         return image, path
 
+    # Applying InceptionV3 (Image Classification Model) takes dataset directory and training image names
+    # 1. Inception v3 has CNN layers for feature extraction from images
+    # 2. Classifier part - consists of a sequence of linear layers, takes feature extraction map and predicts a class.
+    ## Note: for our project, we don't need classifier part
+
+
     def apply_inceptionV3(self, ds_dir, train_image_names):
+
         from tqdm import tqdm
 
-        img_model = tf.keras.applications.inception_v3(include_top=False, weights='imagenet')
+        img_model = tf.keras.applications.InceptionV3(include_top=True,
+                                                      weights="imagenet")
         new_input = img_model.input
         hidden_layers = img_model.layers[-1].output
         img_features_extract_model = tf.keras.Model(new_input, hidden_layers)
@@ -118,7 +144,7 @@ train_img_file_path = "Flickr_8k.trainImages.txt"
 
 captions_text = data_loader.load_text_doc(captions_file_path)
 img_caps_dict = data_loader.image_caption_dict(captions_text)
-training_img_names = data_loader.train_image_names(train_img_file_path)
+training_img_names = data_loader.train_img_names(train_img_file_path)
 
 img_processor.apply_inceptionV3(dataset_dir, training_img_names)
 
